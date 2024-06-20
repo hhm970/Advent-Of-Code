da332e4eac2a462b3ef3934ee7d8e99eb85fe09a
howardman <howardppg@gmail.com>
2024-06-20 14:18:58 +0100

    Created new function get_indices_numbers()

--- 2023/Day_3/part1.py
+++ 2023/Day_3/part1.py
@@ -16,10 +16,31 @@ def get_lines_txt_file() -> list[str]:
 def get_indices_symbols(line: str) -> list[int]:
     """Given a line, returns a list of indices of elements which are not numeric,
     or a period ('.')."""
-    pass
+
+    n = len(line)
+
+    result = []
+
+    for i in range(n):
+        if line[i] != "." and not line[i].isnumeric():
+            result.append(i)
+
+    return result
 
 
-def get_numbers_adj_symbol(line: str) -> list[int]:
+def get_indices_numbers(line: str) -> list[int]:
+    """Given a line, return a list of indices of elements which are numeric."""
+
+    n = len(line)
+
+    result = [i for i in range(n) if line[i].isnumeric()]
+
+    return result
+
+
+def get_numbers_adj_symbol(line: str, above_line_indices: list[int] = [],
+                           below_line_indices: list[int] = [],
+                           current_line_indices: list[int] = []) -> list[int]:
     """Given a line, returns a list of integers which are adjacent to a symbol,
     both above and below (if applicable)."""
     pass
--- 2023/Day_3/test_part1.py
+++ 2023/Day_3/test_part1.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from part1 import get_indices_symbols, get_numbers_adj_symbol
+from part1 import get_indices_symbols, get_indices_numbers, get_numbers_adj_symbol
 
 
 def test_get_indices_symbols_all_periods(sample_line_all_periods):
@@ -18,11 +18,32 @@ def test_get_indices_symbols_all_symbols(sample_line_all_symbols):
 
     n = len(sample_line_all_symbols)
 
-    assert get_indices_symbols(sample_line_all_symbols) == range(n)
+    assert get_indices_symbols(sample_line_all_symbols) == [
+        i for i in range(n)]
 
 
 def test_get_indices_symbols_success(sample_line):
     assert get_indices_symbols(sample_line) == [3, 4, 5, 9, 10, 11, 12]
+
+
+def test_get_indices_numbers_all_periods(sample_line_all_periods):
+    assert get_indices_numbers(sample_line_all_periods) == []
+
+
+def test_get_indices_numbers_all_symbols(sample_line_all_symbols):
+    assert get_indices_numbers(sample_line_all_symbols) == []
+
+
+def test_get_indices_numbers_all_numbers(sample_line_all_numbers):
+
+    n = len(sample_line_all_numbers)
+
+    assert get_indices_numbers(sample_line_all_numbers) == [
+        i for i in range(n)]
+
+
+def test_get_indices_numbers_success(sample_line):
+    assert get_indices_numbers(sample_line) == [0, 1, 2, 6, 7, 8]
 
 
 def test_get_numbers_adj_symbol_all_periods(sample_list_all_periods):
